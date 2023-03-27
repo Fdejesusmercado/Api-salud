e0d1a8d37b245570d85267d8c97649a0905dd0d2
Fabio <fdejesusmercado@unibarranquila.edu.co>
2023-03-26 22:57:30 -0500

    Mejorando API

--- src/app.py
+++ src/app.py
@@ -60,8 +60,13 @@ def cargarPerfil():
             row = cursor.fetchone()
             if row != None:
                 userRetur = UserN(decoded_token['user_id'],'','',row[0]+" "+row[1],row[2])
+                documentos = cargarDocumentos(decoded_token['user_id'])
+                discapacidades = cargarDiscapacidades(decoded_token['user_id'])
+                print(documentos)
                 return jsonify({'fullname':userRetur.fullname,
-                                'cargo':userRetur.cargo
+                                'cargo':userRetur.cargo,
+                                'documentos':documentos,
+                                'discapacidades':discapacidades
                                 })
             else:
                 return None
@@ -72,7 +77,83 @@ def cargarPerfil():
             return
     else:        
         return 
+
+@app.route('/eliminarDiscapacidad',methods = ['GET','POST','PUT']) 
+def eliminarDiscapacidad():
+    cursor = dataBase.connection.cursor()
+    dis_eliminar = request.json['dis_eliminar']
+    print(dis_eliminar)
+    token = request.json['token']
+    decoded_token = jwt.decode(token, 'secreto', algorithms=['HS256'])
    
+    if request.method == 'GET':
+        return jsonify({'status':'Tipo GET'})
+    elif request.method == 'PUT':
+        try:
+            for i in dis_eliminar:
+                print(i)
+                sql = """UPDATE appsalud.discapacidades SET estado = 2  
+                        WHERE tipo_discapacidad_idtipo_discapacidad = {} and usuarios_idusuarios ={} """.format(i,decoded_token['user_id'])
+                print(sql)
+                cursor.execute(sql)
+                dataBase.connection.commit()
+                
+            return jsonify({'status':'Discapacidad Eliminada'})
+        except:
+            return jsonify({'status':'Entro al except'})
+        
+            
+
+
+
+def cargarDocumentos(idUser):
+     
+     cursor = dataBase.connection.cursor()
+     sql = """select appsalud.usuarios.nombre, apellido, nombreRol,url, appsalud.documentos.nombre_documento, appsalud.tipo_documento.nombre AS EXTENCION from appsalud.usuarios 
+     inner join  appsalud.rol on rol.idrol = usuarios.rol_idrol
+     inner join appsalud.documentos on  documentos.usuarios_idusuarios = usuarios.idusuarios
+     inner join appsalud.tipo_documento on documentos.id_tipo_documento = tipo_documento.idtipo_documento
+     where idusuarios = {}""".format(idUser)
+     cursor.execute(sql)
+     row = cursor.fetchall()
+     doc = []
+     if row != None:  
+        for i in row:
+            doc.append({
+                'nombreDoc':i[4],
+                'tipoDoc':i[5],
+                'URL':i[3]
+            })
+            
+        return doc
+        
+          
+     else:
+         return jsonify({'status': 'err'})
+     
+def cargarDiscapacidades(idUser):
+     
+     cursor = dataBase.connection.cursor()
+     sql = """select idtipo_discapacidad, nombre, descripcion from appsalud.discapacidades
+              inner join  appsalud.tipo_discapacidad on discapacidades.tipo_discapacidad_idtipo_discapacidad = tipo_discapacidad.idtipo_discapacidad
+              where usuarios_idusuarios = {} and discapacidades.estado = 1 and tipo_discapacidad.estado = 1""".format(idUser)
+     cursor.execute(sql)
+     row = cursor.fetchall()
+     dis = []
+     if row != None:  
+        for i in row:
+            dis.append({
+                'idDiscapacidad':i[0],
+                'nombreDis':i[1],
+                'descripcionDis':i[2],
+            })
+            
+        return dis
+        
+          
+     else:
+         return jsonify({'status': 'err'})
+         
 
 # funcion para generar token
 def get_token(idUser,userName):
